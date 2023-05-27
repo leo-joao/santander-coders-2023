2ab889cec2495c51fc0590c2f73b07e7668d6d8d
Léo João <leo_joao69@hotmail.com>
2023-05-27 10:53:01 -0300

    começando no javascript

---
+++ JavaScript/operadores.js
@@ -0,0 +1,27 @@
+// Operadores aritmeticos
+
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+// Adição              +
+// Subtração           -
+// Multiplicação       *
+// Divisão             /
+// Exponenciação       **
+// Resto da Divisão    %
+
+const soma = primeiroNumero + segundoNumero;
+
+console.log('Soma =', soma);
+console.log('Subtração =', primeiroNumero - segundoNumero);
+console.log('Multiplicação =', primeiroNumero * segundoNumero);
+console.log('Divisão =', primeiroNumero / segundoNumero);
+console.log('Exponenciação =', primeiroNumero ** segundoNumero);
+console.log('Resto da Divisão =', primeiroNumero % 3);
+
+// Segue a mesma lógica de precedência da matemática igual as outras linguagens
+console.log(3 + 7 * 3);
+
+// Bibioteca Math
+
+console.log('Raiz quadrada de 16 = ', Math.sqrt(16));
---
+++ JavaScript/variaveis.js
@@ -0,0 +1,35 @@
+// Variavéis
+// Ponto e vírgula é opcional
+
+let idade = 31;
+
+console.log((idade * 10)/20);
+
+// Tipos de variáveis
+
+idade = 25;
+let numero = 30;        // number
+let altura = 1.77;      // number
+let nome = 'Léo';       // string
+let verifica = true;    // boolean
+
+console.log(idade, typeof idade);
+console.log(numero);
+console.log(altura);
+console.log(nome);
+console.log(verifica, typeof verifica);
+
+let outra;  // undefined
+
+console.log(outra);
+
+let nula = null;
+
+console.log(nula);
+
+var variavel = 15;  // maneira antiga de declarar variáveis, obsoleto
+
+const constante = 100;
+const pi = 3.14159265358979323846;
+
+console.log(pi);
