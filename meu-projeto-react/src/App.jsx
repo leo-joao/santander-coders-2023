c4a1033910ab717aa4a6551a8371666b861a616e
Léo João <leo_joao69@hotmail.com>
2023-05-28 12:09:30 -0300

    buscando dados e listando

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,54 +1,42 @@
-import MeuContador from "./components/MeuContador"
-import MinhaLista from "./components/MinhaLista"
-import { useState } from "react";
-import { useEffect} from "react";
-import { toLoweCase } from "react";
+import { useEffect, useState } from "react"
 
-const minhaLista = [
-  { id: "1", value: "Fruta" },
-  { id: "2", value: "Legume" },
-  { id: "3", value: "Carne" },
+const tarefas = [
+    { id: "1", title: "minha primeira tarefa"},
+    { id: "2", title: "minha segunda tarefa"},
+    { id: "3", title: "minha terceira tarefa"},
+    { id: "4", title: "minha quarta tarefa"},
 ]
 
-function App() {
+export default function App() {
 
-  const [produtos, setProdutos] = useState(minhaLista)
-  const [pesquisa, setPesquisa] = useState('')
+    const [tarefas, setTarefas] = useState([])
 
-  useEffect(
-    () => {
-      if (pesquisa) {
-        const novaLista = minhaLista.filter( (item) => {
-          return item.value.toString().toLowerCase().includes(pesquisa.toString().toLowerCase())
-        })
-        setProdutos(novaLista)
-      } else {
-        setProdutos(minhaLista)
-      }
-    }
-  , [pesquisa])
+    useEffect(() => {
+        async function buscarDados() {
+            const resultado = await fetch("https://jsonplaceholder.typicode.com/todos")
+            const resultadoFinal = await resultado.json()
+            return resultadoFinal
+        }
 
+        buscarDados().then(res => setTarefas(res))
 
-  return (
-    <div>
-      <MeuContador />
-      <br></br>
-      <h1>Efeitos Colaterais</h1>
+    }, [])
 
-      <input
-        value={pesquisa}
-        onChange={(e) => setPesquisa(e.target.value)}
-
-        placeholder="pesquise aqui"></input>
-      {produtos.map((item) => {
-        return (
-          <div key={item.id}>
-            <h4>{item.value}</h4>
-          </div>
-        )
-      })}
-    </div>
-  )
+    return (
+        <div>
+            <h1>Buscando dados</h1>
+            <ol>
+            {tarefas.map((tarefa) => {
+                return (
+                    <div>
+                        <li key={tarefa.id}>
+                            {tarefa.title}
+                            {tarefa.completed ? <span> - Tarefa Concluída</span> : null}
+                        </li>
+                    </div>
+                    )
+                })}
+            </ol>
+        </div>
+    )
 }
-
-export default App
