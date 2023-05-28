f3e176c2c98dcaa480ddeb7e554e861a80eb73b7
Léo João <leo_joao69@hotmail.com>
2023-05-28 09:17:57 -0300

    aprendendo react

---
+++ meu-projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,5 @@
+export default function MeuComponente() {
+    return (
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni voluptates ea velit reprehenderit inventore animi possimus aspernatur distinctio similique totam architecto, ad nobis quas natus, a, ab recusandae praesentium dignissimos.</p>
+    )
+  }
---
+++ meu-projeto-react/src/components/MeuContador.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function MeuContador() {
+    
+    const [contador, setContador] = useState(0); 
+    
+    function aumentar() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <h1>Meu contador</h1>
+            <h3>{contador}</h3>
+            <button onClick={aumentar}>Add</button>
+        </div>
+    )
+}
