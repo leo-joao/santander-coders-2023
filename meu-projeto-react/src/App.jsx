b884a59c2485a31caacbd98803267356c8d6f77d
Léo João <leo_joao69@hotmail.com>
2023-05-28 18:19:38 -0300

    publicando na vercel

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,22 +1,42 @@
-import { useState } from "react";
-import "./styles.css"
-import styles from "./button.module.css"
+import { useEffect, useState } from "react"
 
-function App() {
+const tarefas = [
+    { id: "1", title: "minha primeira tarefa"},
+    { id: "2", title: "minha segunda tarefa"},
+    { id: "3", title: "minha terceira tarefa"},
+    { id: "4", title: "minha quarta tarefa"},
+]
 
-    const [contador, setContador] = useState(0)
+export default function App() {
 
-    function aumentar() {
-        setContador(contador + 1)
-    }
+    const [tarefas, setTarefas] = useState([])
 
-  return (
-    <div className="container">
-        <h1>Meu Contador</h1>
-        <h3>{contador}</h3>
-        <button className={styles.myButton} onClick={aumentar}>Aumentar</button>
-    </div>
-  )
+    useEffect(() => {
+        async function buscarDados() {
+            const resultado = await fetch("https://jsonplaceholder.typicode.com/todos")
+            const resultadoFinal = await resultado.json()
+            return resultadoFinal
+        }
+
+        buscarDados().then(res => setTarefas(res))
+
+    }, [])
+
+    return (
+        <div>
+            <h1>Buscando dados</h1>
+            <ol>
+            {tarefas.map((tarefa) => {
+                return (
+                    <div>
+                        <li key={tarefa.id}>
+                            {tarefa.title}
+                            {tarefa.completed ? <span> - Tarefa Concluída</span> : null}
+                        </li>
+                    </div>
+                    )
+                })}
+            </ol>
+        </div>
+    )
 }
-
-export default App
